4ccd440d274fe65b3ed588d5e7416f379c2a6410
DineshKalluri5296 <dinesh.kalluri@outlook.com>
2026-08-26 09:34:07 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -28,12 +28,12 @@ MODEL_PATH = "model/model.pkl"
 
 MLFLOW_TRACKING_URI = os.getenv(
     "MLFLOW_TRACKING_URI",
-    "http://34.200.234.63:5000"
+    "http://100.60.81.211:5000"
 )
 
 EXPERIMENT_NAME = os.getenv(
     "MLFLOW_EXPERIMENT",
-    "LungCancerPrediction22"
+    "LungCancerPrediction212"
 )
 
 REGISTERED_MODEL_NAME = "LungCancerModelprediction"
